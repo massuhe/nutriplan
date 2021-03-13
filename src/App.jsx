281c543c9b55e7cd2f31922befdd50f8cca16b0a
Esteban <massuhe@outlook.com>
2021-03-13 19:36:04 -0300

    Initial plan carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header.js';
 import Plan from './components/Plan.js';
 import PlanActions from './components/PlanActions.js';
+import planMock from './data/plans.json';
+
+const useMock = true;
 
 function App() {
+  const [plan, setPlan] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (useMock) {
+      setPlan(planMock);
+    }
+    setLoading(false);
+  }, []);
+
   return (
     <>
       <Header />
       <main className="h-full bg-green-500 flex flex-col">
         <PlanActions />
-        <Plan />
+        <Plan plan={plan} loading={loading} />
       </main>
     </>
   );
--- src/components/PlanDay.jsx
+++ src/components/PlanDay.jsx
@@ -1,43 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-const PlanDay = ({ active }) => {
-  console.log({ active });
+const toDisplayDate = (date) => {
+  return new Date(date).toDateString();
+};
+
+const PlanDay = ({ planDay, offset }) => {
   return (
     <article
-      className={`bg-green-50 py-3 px-5 flex flex-col max-w-sm rounded-lg shadow-2xl mx-2 ${
-        !active ? 'my-10 w-1/6' : 'flex-1'
-      }`}
+      className="bg-green-50 py-3 px-5 flex flex-col rounded-lg shadow-2xl my-10"
+      style={{
+        minWidth: '18.68%',
+        width: '18.68%',
+        marginLeft: '1.1%',
+        transform: `translateX(calc(var(--offset) * 105.3533%)`,
+      }}
     >
       <h3 className="text-center font-serif text-lg text-green-500 mb-5">
-        Miércoles 16/02
+        {toDisplayDate(planDay.date)}
       </h3>
       <div className="flex flex-col flex-1">
         <section className="flex-1">
           <h4 className="font-semibold text-blue-500">Desayuno</h4>
-          <p className="text-green-900">
-            Vaso de leche + panqueque de vainilla + miel + frutas a elección
-          </p>
+          <p className="text-green-900">{planDay.meals[0].value}</p>
         </section>
         <section className="flex-1">
           <h4 className="font-semibold text-red-500">Almuerzo</h4>
-          <p className="text-green-900">
-            Milanesa de soja con queso y rodaja de tomate arriba + ensalada de
-            repollo y zanahoria
-          </p>
+          <p className="text-green-900">{planDay.meals[1].value}</p>
         </section>
         <section className="flex-1">
           <h4 className="font-semibold text-yellow-500">Merienda</h4>
-          <p className="text-green-900">
-            Yogurt + 2 rodajas de pan integral con queso untable + semillas +
-            manzana
-          </p>
+          <p className="text-green-900">{planDay.meals[2].value}</p>
         </section>
         <section className="flex-1">
           <h4 className="font-semibold text-indigo-500">Cena</h4>
-          <p className="text-green-900">
-            Omelette (2 huevos) de espinaca con queso, tomate y arvejas +
-            ensalada de zanahoria, choclo y tomate
-          </p>
+          <p className="text-green-900">{planDay.meals[3].value}</p>
         </section>
       </div>
     </article>
